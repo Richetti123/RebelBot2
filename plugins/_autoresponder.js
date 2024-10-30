ba4d142b061b7f919d8d6d091d6aeecee7980a38
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-10-30 20:16:07 +0100

    Update _autoresponder.js

--- plugins/_autoresponder.js
+++ plugins/_autoresponder.js
@@ -7,7 +7,7 @@ let chat = global.db.data.chats[m.chat]
 if (m.mentionedJid.includes(this.user.jid) && m.isGroup && !chat.isBanned) {
 let stiker = await sticker(imagen1, false, global.packname, global.author)  
 this.sendFile(m.chat, stiker, 'sticker.webp', null, m, false, { 
-contextInfo: { externalAdReply: { title: wm, body: wm, sourceUrl: md, thumbnail: imagen2}}})}
+contextInfo: { externalAdReply: { title: wm, body: wm, sourceUrl: md, thumbnail: "./Menu2.jpg"}}})}
     
 return !0 }
 export default handler
