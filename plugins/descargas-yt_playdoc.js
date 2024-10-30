54e591b070c157f8c8a4ae9c05f8b8e999a73087
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-10-30 20:12:09 +0100

    Add files via upload

--- plugins/descargas-yt_playdoc.js
+++ plugins/descargas-yt_playdoc.js
@@ -2,8 +2,7 @@ import fetch from 'node-fetch';
 import yts from 'yt-search';
 import ytdl from 'ytdl-core';
 import axios from 'axios';
-import { ytDownload } from '../lib/y2mate.js';
-
+import {youtubedl, youtubedlv2} from '@bochilteam/scraper';
 const handler = async (m, {conn, command, args, text, usedPrefix}) => {
 if (!args || !args[0]) return conn.reply(m.chat, `${lenguajeGB['smsAvisoMG']()}${mid.smsMalused7}\n*${usedPrefix + command} https://youtu.be/85xI8WFMIUY*`, fkontak, m)
 try { 
@@ -17,30 +16,24 @@ additionalText = '𝙑𝙄𝘿𝙀𝙊'
 conn.reply(m.chat, `${lenguajeGB['smsAvisoEG']()}𝙋𝙍𝙊𝙉𝙏𝙊 𝙏𝙀𝙉𝘿𝙍𝘼 𝙎𝙐 𝘿𝙊𝘾𝙐𝙈𝙀𝙉𝙏𝙊 ${additionalText}, 𝙀𝙎𝙋𝙀𝙍𝙀 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍\n\n𝙎𝙊𝙊𝙉 𝙔𝙊𝙐 𝙒𝙄𝙇𝙇 𝙃𝘼𝙑𝙀 𝙔𝙊𝙐𝙍 ${additionalText} 𝘿𝙊𝘾𝙐𝙈𝙀𝙉𝙏, 𝙋𝙇𝙀𝘼𝙎𝙀 𝙒𝘼𝙄𝙏`, fkontak,  m)
 if (command == 'playaudiodoc' || command == 'ytmp3doc') {
 try {
-	let v = yt_play[0].url;
-	let data = await fetch('https://www.vanitas-api.online/download/youtube-audio?url=' + encodeURIComponent(v) + '&apikey=richetti').then((data) => data.json()).then((res) => res);
-	let cap = `╭━❰  ${wm}  ❱━⬣\n┃📥 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝘿𝙇 📥\n┃ও *${mid.smsYT1}:* \n┃﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘\n┃ও *${mid.smsYT11}:*\n╰━━━━━❰ *𓃠 ${vs}* ❱━━━━⬣`.trim();
-	await conn.sendMessage(m.chat, {
-		document: { 
-			url: data.response.link
-		},
-		fileName: `${data.response.title}.mp3`,
-		caption: cap,
-		mimetype: 'audio/mpeg',
-		contextInfo: {
-			externalAdReply: {
-				title: data.resultado.title,
-				body: "",
-				thumbnailUrl: data.resultado.image, 
-				mediaType: 1,
-				showAdAttribution: true,
-				renderLargerThumbnail: true
-			}
-		}
-	} , { quoted: m });
-	handler.limit = 1
-} catch(e) {
-	console.log(e);
+const q = '128kbps';
+const v = yt_play[0].url;
+const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+const dl_url = await yt.audio[q].download();
+const ttl = await yt.title;
+const size = await yt.audio[q].fileSizeH;
+let cap = `╭━❰  ${wm}  ❱━⬣\n┃📥 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝘿𝙇 📥\n┃ও *${mid.smsYT1}:* \n┃» ${ttl}\n┃﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘\n┃ও *${mid.smsYT11}:*\n┃» ${size}\n╰━━━━━❰ *𓃠 ${vs}* ❱━━━━⬣`.trim()
+await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: cap, mimetype: 'audio/mpeg', contextInfo: {
+externalAdReply: {
+title: ttl,
+body: "",
+thumbnailUrl: yt_play[0].thumbnail, 
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}} , { quoted: m })   
+handler.limit = 1
+} catch {
 try {
 const lolhuman = await fetch(`https://api.lolhuman.xyz/api/ytaudio2?apikey=${lolkeysapi}&url=${yt_play[0].url}`);
 const lolh = await lolhuman.json();
@@ -76,28 +69,23 @@ handler.limit = 2
 }}}}
 if (command == 'playvideodoc' || command == 'ytmp4doc') {
 try {
-	let v = yt_play[0].url;
-	let data = await fetch('https://www.vanitas-api.online/download/youtube-video?url=' + encodeURIComponent(v) + '&apikey=richetti').then((data) => data.json()).then((res) => res);
-	let cap = `╭━❰  ${wm}  ❱━⬣\n┃📥 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝘿𝙇 📥\n┃ও *${mid.smsYT1}:* \n┃﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘\n┃ও *${mid.smsYT11}:*\n╰━━━━━❰ *𓃠 ${vs}* ❱━━━━⬣`.trim();
-	await conn.sendMessage(m.chat, {
-		document: { 
-			url: data.resultado.download.audio
-		},
-		fileName: `${data.resultado.title}.mp3`,
-		caption: cap,
-		mimetype: 'audio/mpeg',
-		contextInfo: {
-			externalAdReply: {
-				title: data.resultado.title,
-				body: "",
-				thumbnailUrl: data.resultado.image, 
-				mediaType: 1,
-				showAdAttribution: true,
-				renderLargerThumbnail: true
-			}
-		}
-	} , { quoted: m });
-	handler.limit = 2
+const qu = '360';
+const q = qu + 'p';
+const v = yt_play[0].url;
+const yt = await youtubedl(v).catch(async (_) => await youtubedlv2(v));
+const dl_url = await yt.video[q].download();
+const ttl = await yt.title;
+const size = await yt.video[q].fileSizeH;
+await conn.sendMessage(m.chat, { document: { url: dl_url }, caption: `╭━❰  ${wm}  ❱━⬣\n┃📥 𝙔𝙊𝙐𝙏𝙐𝘽𝙀 𝘿𝙇 📥\n┃ও *${mid.smsYT1}:* \n┃» ${ttl}\n┃﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘﹘\n┃ও *${mid.smsYT11}:*\n┃» ${size}\n╰━━━━━❰ *𓃠 ${vs}* ❱━━━━⬣`, fileName: `${ttl}.mp3`, mimetype: 'audio/mpeg', contextInfo: {
+externalAdReply: {
+title: ttl,
+body: "",
+thumbnailUrl: yt_play[0].thumbnail, 
+mediaType: 1,
+showAdAttribution: true,
+renderLargerThumbnail: true
+}}} , { quoted: m })   
+handler.limit = 2
 } catch {
 try {
 const mediaa = await ytMp4(yt_play[0].url);
