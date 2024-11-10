7fcea31ac093c465a7b211f3fd0815c51b58af0c
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-11-10 23:51:06 +0100

    Update descargas-yt_play.js

--- plugins/descargas-yt_play.js
+++ plugins/descargas-yt_play.js
@@ -53,12 +53,31 @@ let handler = async (m, { conn, command, args, text, usedPrefix }) => {
 					try {
 						let v = yt_play[0].url
 						const yt = await ytDownload(v, 'audio');
-						await conn.sendMessage(m.chat, { audio: { url: yt }, mimetype: 'audio/mpeg', contextInfo: null}, { quoted: m });
+						await conn.sendMessage(m.chat, { audio: { url: yt }, mimetype: 'audio/mpeg', contextInfo: {
+							externalAdReply: {
+							title: yt_play[0].title,
+							body: "",
+							thumbnailUrl: yt_play[0].thumbnail, 
+							mediaType: 1,
+							showAdAttribution: true,
+							renderLargerThumbnail: true
+						}}} , { quoted: m });
 					} catch {
 						try {
 							const dataRE = await fetch(`https://www.vanitas-api.online/download/youtube-audio?url=${yt_play[0].url}`);
 							const dataRET = await dataRE.json();
-							await conn.sendMessage(m.chat, { audio: { url: yt }, mimetype: 'audio/mpeg', contextInfo: null}, { quoted: m });
+							await conn.sendMessage(m.chat, { audio: { url: dataRET.response.link }, mimetype: 'audio/mpeg',
+								contextInfo: {
+									externalAdReply: {
+										title: yt_play[0].title,
+										body: "",
+										thumbnailUrl: yt_play[0].thumbnail,
+										mediaType: 1,
+										showAdAttribution: true,
+										renderLargerThumbnail: true
+									}
+								}
+							} , { quoted: m });
 } catch {
 try {
 let humanLol = await fetch(`https://api.lolhuman.xyz/api/ytplay?apikey=${lolkeysapi}&query=${yt_play[0].title}`)
